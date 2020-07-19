8eda3a22ac1ac66db246517b8895f2e76598c835
libao12345 <1349084222@qq.com>
2020-07-19 17:00:18 +0800

    chenge

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/swiper-bundle.css'
+
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)  //解决移动端点击事件延迟300毫秒的问题
+Vue.use(VueAwesomeSwiper, )
 
 new Vue({
   router,
