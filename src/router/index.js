5c119a6b1a20cc7d03d25a3fbfb94cfeabd6b0c1
libao12345 <1349084222@qq.com>
2020-07-20 20:26:50 +0800

    city router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/City'
 
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+  const routes = [{
+      path: '/',
+      name: 'Home',
+      component: Home
+    },{
+      path: '/city',
+      name: 'City',
+      component: City
+    }
+  ]
 
 const router = new VueRouter({
   mode: 'history',
